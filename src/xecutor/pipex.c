fa7483c6bb5f6eb0cb67d50ee89933e6e6f09db6
Martin Cutura <mcutura@student.42berlin.de>
2023-07-08 17:34:49 +0200

    fix mario&luigi leaking fds

--- src/xecutor/pipex.c
+++ src/xecutor/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mcutura <mcutura@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 13:34:17 by mcutura           #+#    #+#             */
-/*   Updated: 2023/07/08 14:02:27 by mcutura          ###   ########.fr       */
+/*   Updated: 2023/07/08 17:33:32 by mcutura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,10 @@ static int	wait_for_children(pid_t *pids, int len)
 static int	pipeline_len(t_cmd *cmd)
 {
 	int		len;
-	t_cmd	*node;
 
 	len = 0;
-	node = cmd;
-	while (node)
-	{
-		len++;
-		node = node->pipe;
-	}
+	while (cmd && ++len)
+		cmd = cmd->pipe;
 	return (len);
 }
 
@@ -47,14 +42,11 @@ static int	supermario(t_cmd *cmd, int i, int len, int fd[2][2])
 	char	**args;
 	char	*path;
 
-	if (i)
-		if (dup2(fd[i & 1][0], STDIN_FILENO) == -1)
-			exit(EXIT_FAILURE);
-	if (i < len - 1)
-		if (dup2(fd[(i + 1) & 1][1], STDOUT_FILENO) == -1)
-			exit(EXIT_FAILURE);
-	if (close(fd[0][0]) || close(fd[0][1]) || \
-		close(fd[1][0]) || close(fd[1][1]))
+	if (i && (dup2(fd[(i - 1) & 1][0], STDIN_FILENO) == -1 || \
+		close(fd[(i - 1) & 1][1]) || close(fd[(i - 1) & 1][0])))
+		exit(EXIT_FAILURE);
+	if (i < len - 1 && (dup2(fd[i & 1][1], STDOUT_FILENO) == -1 || \
+		close(fd[i & 1][0]) || close(fd[i & 1][1])))
 		exit(EXIT_FAILURE);
 	if (is_builtin(cmd))
 		exit(execute_builtin(cmd));
@@ -75,14 +67,24 @@ static int	luigi(int i, int len, int fd[2][2])
 {
 	if (i == len - 1)
 	{
-		if (close(fd[(i) & 1][0]) || close(fd[(i) & 1][1]))
+		if (i > 1 && (close(fd[i & 1][0]) || close(fd[i & 1][1])))
 			return (EXIT_FAILURE);
 		return (EXIT_SUCCESS);
 	}
-	if (i && (close(fd[(i) & 1][0]) || close(fd[(i) & 1][1])))
-		return (EXIT_FAILURE);
-	if (i < len - 1 && (pipe(fd[(i) & 1]) == -1))
-		return (EXIT_FAILURE);
+	if (i & 1)
+	{
+		if (i > 1 && (close(fd[1][0]) || close(fd[1][1])))
+			return (EXIT_FAILURE);
+		if (pipe(fd[1]))
+			return (EXIT_FAILURE);
+	}
+	else
+	{
+		if (i && (close(fd[0][0]) || close(fd[0][1])))
+			return (EXIT_FAILURE);
+		if (pipe(fd[0]))
+			return (EXIT_FAILURE);
+	}
 	ft_wait(100000);
 	return (EXIT_SUCCESS);
 }
@@ -94,23 +96,23 @@ int	pipex(t_cmd *cmd)
 	pid_t		*pids;
 	int			i;
 
-	if (pipe(fd[0]) == -1 || pipe(fd[1]) == -1)
-		return (EXIT_FAILURE);
 	pids = malloc(sizeof(pid_t) * pipelen);
 	if (!pids)
 		return (EXIT_FAILURE);
 	i = -1;
 	while (++i < pipelen)
 	{
+		if (luigi(i, pipelen, fd))
+			return (free(pids), ft_dprintf(STDERR_FILENO, "%s %s: %s", MISH, \
+				"pipex", strerror(errno)), EXIT_FAILURE);
 		pids[i] = fork();
 		if (pids[i] < 0 || (!pids[i] && supermario(cmd, i, pipelen, fd)))
-			return (free(pids), ft_dprintf(STDERR_FILENO, "%s: %s: %s", MISH, \
-				"pipex", strerror(errno)), EXIT_FAILURE);
-		if (luigi(i, pipelen, fd))
-			return (free(pids), ft_dprintf(STDERR_FILENO, "%s: %s: %s", MISH, \
+			return (free(pids), ft_dprintf(STDERR_FILENO, "%s %s: %s", MISH, \
 				"pipex", strerror(errno)), EXIT_FAILURE);
 		cmd = cmd->pipe;
 	}
+	if (close(fd[i & 1][0]) || close(fd[i & 1][1]))
+		ft_dprintf(STDERR_FILENO, "%s %s: %s", MISH, "pipex", strerror(errno));
 	signal_suspend();
 	i = wait_for_children(pids, pipelen);
 	return (signal_restore(), free(pids), i);
